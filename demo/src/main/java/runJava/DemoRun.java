129653cca89aca1b8677721bb3bdac867fc3f8c0
jbc0503 <jbc0503@lgcns.com>
2022-06-16 18:05:00 +0900

    Demo Run

---
+++ demo/src/main/java/runJava/DemoRun.java
@@ -0,0 +1,13 @@
+package runJava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoRun {
+
+	public static void main(String[] args) {
+		System.out.println("Server running..");
+		//SpringApplication.run(DemoApplication.class, args);
+	}
+}
